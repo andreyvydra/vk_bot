d3e982e592f99b24f8b48171a15c8e492787e178
shmonder-glitch <vidra8888@yandex.ru>
2020-08-19 17:06:00 +0300

    rsg
    Ну шо пацаны первая версия

--- longpool.py
+++ longpool.py
@@ -3,28 +3,19 @@ import requests
 import vk_api
 import pymorphy2
 from vk_api.longpoll import VkLongPoll, VkEventType
-from bot import VkBot
 import json
-from main import get_message
+from weather import get_message
 
 
 def get_keyboard(inline=False):
     keyboard = {
-    "one_time": False,
-    "buttons": [
-        [{
+        "one_time": False,
+        "buttons": [
+            [{
                 "action": {
                     "type": "text",
                     "payload": "{\"button\": \"10\"}",
-                    "label": "Погода на день"
-                },
-                "color": "negative"
-            }],
-               [{
-                "action": {
-                    "type": "text",
-                    "payload": "{\"button\": \"10\"}",
-                    "label": "Погода на утро"
+                    "label": "Погода на весь день"
                 },
                 "color": "positive"
             }],
@@ -32,15 +23,39 @@ def get_keyboard(inline=False):
                 "action": {
                     "type": "text",
                     "payload": "{\"button\": \"10\"}",
-                    "label": "Погода на вечер"
+                    "label": "День"
                 },
                 "color": "primary"
-            }],
+            },
+                {
+                    "action": {
+                        "type": "text",
+                        "payload": "{\"button\": \"10\"}",
+                        "label": "Утро"
+                    },
+                    "color": "primary"
+                }],
             [{
                 "action": {
                     "type": "text",
                     "payload": "{\"button\": \"10\"}",
-                    "label": "Погода на ночь"
+                    "label": "Вечер"
+                },
+                "color": "primary"
+            },
+                {
+                    "action": {
+                        "type": "text",
+                        "payload": "{\"button\": \"10\"}",
+                        "label": "Ночь"
+                    },
+                    "color": "primary"
+                }],
+            [{
+                "action": {
+                    "type": "text",
+                    "payload": "{\"button\": \"10\"}",
+                    "label": "Погода в реальном времени"
                 },
                 "color": "secondary"
             }],
@@ -50,43 +65,51 @@ def get_keyboard(inline=False):
                     "payload": "{\"button\": \"10\"}",
                     "label": "Выбрать город"
                 },
-                "color": "primary"
+                "color": "negative"
             }]
-    ],
-    "inline": inline
+        ],
+        "inline": inline
     }
     keyboard = json.dumps(keyboard, ensure_ascii=False).encode('utf-8')
     keyboard = str(keyboard.decode('utf-8'))
     return keyboard
 
 
-def write_msg(user_id, city, time=3):
-    vk.method('messages.send', {'user_id': user_id, 'message': get_message(city, time), 'random_id': vk_api.utils.get_random_id()})
+def write_msg_with_forecast(user_id, city, time=25):
+    vk.method('messages.send',
+              {'user_id': user_id, 'message': get_message(city, time), 'random_id': vk_api.utils.get_random_id()})
+
+
+def write_reminder(user_id):
+    vk.method('messages.send', {'user_id': user_id, 'message':
+                                                               'Спасибо за использование данного бота :)&#10084;\n'
+                                                               'Катя яя люблю тебяяяя11111111111111111',
+              'random_id': vk_api.utils.get_random_id()})
 
 
 def write_weather_buttons(user_id):
-    vk.method('messages.send', {'user_id': user_id, 'message': 'Выберите время или город &#128336;&#127751;', 'keyboard': get_keyboard(True),
+    vk.method('messages.send', {'user_id': user_id, 'message': 'Выберите время или город &#128336;&#127751;',
+                                'keyboard': get_keyboard(True),
                                 'random_id': vk_api.utils.get_random_id()})
 
 
 def write_incorrect_msg(user_id):
-    vk.method('messages.send', {'user_id': user_id, 'message': 'Несуществующая команда команда &#10060;'
-                                                               ' Пожалуйста, повторите попытку или введите инфо,'
-                                                               ' чтобы получить список команд &#9940;&#9888;',
-                                'random_id': vk_api.utils.get_random_id()})
+    vk.method('messages.send', {'user_id': user_id, 'message': 'Несуществующая команда команда &#10060;\n'
+                                                               '&#10071;&#10071;&#10071; Спешим напомнить вам, что получить список действий'
+                                                               ' вы можете с помощью команды "погода" &#10071;&#10071;&#10071;\n',
+                                'random_id': vk_api.utils.get_random_id(), 'keyboard': get_keyboard()})
     print('Error: Incorrect input')
 
 
 def write_incorrect_city(user_id):
-    vk.method('messages.send', {'user_id': user_id, 'message': 'Некорректное название &#10060; Повторите попытку'
-                                                               ' введя команду "Выбрать город" или нажмите'
-                                                               ' на аналогичную кнопку &#9940;&#9888;',
-                                'random_id': vk_api.utils.get_random_id()})
+    vk.method('messages.send', {'user_id': user_id, 'message': 'Некорректное название &#10060;  \n',
+                                'random_id': vk_api.utils.get_random_id(), 'keyboard': get_keyboard()})
 
 
 def write_correct_city(user_id):
     vk.method('messages.send', {'user_id': user_id, 'message': 'Город успешно изменён &#9989;&#127961;',
-                      'random_id': vk_api.utils.get_random_id()})
+                                'random_id': vk_api.utils.get_random_id()})
+
 
 def write_city_choice(user_id):
     vk.method('messages.send', {'user_id': user_id, 'message': 'Введите город или населённый пункт &#127747;',
@@ -102,16 +125,17 @@ def write_info(user_id):
                                                                ' которые ты можешь выполнить &#128172;\n'
                                                                '&#11015;&#11015;&#11015;&#11015;&#11015;&#11015;&#11015'
                                                                ';&#11015;&#11015;',
-                                'random_id': vk_api.utils.get_random_id()})
+                                'random_id': vk_api.utils.get_random_id(), 'keyboard': get_keyboard()})
+
 
 
 def output_code(code):
     print(f'Code: {code}')
 
 
-token = "ba6713c40627ba9281241f8b6471c205ef2947306d20d44fe234454b5352d4aecdac76d24ef7a11bbf3c9"
+TOKEN = "ba6713c40627ba9281241f8b6471c205ef2947306d20d44fe234454b5352d4aecdac76d24ef7a11bbf3c9"
 
-vk = vk_api.VkApi(token=token)
+vk = vk_api.VkApi(token=TOKEN)
 
 longpoll = VkLongPoll(vk)
 
@@ -127,15 +151,20 @@ for event in longpoll.listen():
         if event.to_me:
 
             if event.user_id not in users:
-                users[event.user_id] = vk.method('users.get', {'user_id': event.user_id,
-                                                               'fields': ['city']})[0]['city']['title']
+                city = vk.method('users.get', {'user_id': event.user_id,
+                                               'fields': ['city']})[0]
+                
+                if 'city' in city:
+                    users[event.user_id] = city['city']['title']
+                else:
+                    users[event.user_id] = 'Санкт-Петербург'
 
-            bot = VkBot(event.user_id)
             if city_choice:
                 city_choice = False
-                request = requests.get("https://sinoptik.com.ru/погода-" + users[event.user_id].lower())
+                request = requests.get("https://sinoptik.com.ru/погода-" + event.text.lower())
                 b = bs4.BeautifulSoup(request.text, "html.parser")
-                if b.select('.r404'):
+                r = b.select('.r404')
+                if r:
                     output_code(-1)
                     write_incorrect_city(event.user_id)
                 else:
@@ -148,33 +177,39 @@ for event in longpoll.listen():
             elif event.text.lower() == 'погода':
                 write_weather_buttons(event.user_id)
                 output_code(1)
-            elif event.text.lower() == 'погода на ночь':
-                write_msg(event.user_id, users[event.user_id])
+            elif event.text.lower() == 'ночь':
+                write_msg_with_forecast(event.user_id, users[event.user_id], 3)
                 output_code(1)
-            elif event.text.lower() == 'погода на утро':
-                write_msg(event.user_id, users[event.user_id], 9)
+                write_reminder(event.user_id)
+            elif event.text.lower() == 'утро':
+                write_msg_with_forecast(event.user_id, users[event.user_id], 9)
                 output_code(1)
-            elif event.text.lower() == 'погода на день':
-                write_msg(event.user_id, users[event.user_id], 15)
+                write_reminder(event.user_id)
+            elif event.text.lower() == 'день':
+                write_msg_with_forecast(event.user_id, users[event.user_id], 15)
                 output_code(1)
-            elif event.text.lower() == 'погода на вечер':
-                write_msg(event.user_id, users[event.user_id], 21)
+                write_reminder(event.user_id)
+            elif event.text.lower() == 'вечер':
+                write_msg_with_forecast(event.user_id, users[event.user_id], 21)
                 output_code(1)
+                write_reminder(event.user_id)
+            elif event.text.lower() == 'погода на весь день':
+                write_msg_with_forecast(event.user_id, users[event.user_id])
+                output_code(1)
+                write_reminder(event.user_id)
             elif event.text.lower() == 'выбрать город':
                 write_city_choice(event.user_id)
                 city_choice = True
+            elif event.text.lower() == 'погода в реальном времени':
+                write_msg_with_forecast(event.user_id, users[event.user_id], 26)
+                output_code(1)
+                write_reminder(event.user_id)
             else:
                 output_code(0)
                 write_incorrect_msg(event.user_id)
             '''elif True:
                 city = vk.method('users.get', {'user_id': event.user_id, 'fields': ['city']})[0]['city']['title']
                 write_msg(event.user_id, city)'''
-
-
-
-            # msg = bot._get_weather()
-            # if msg:
-            # write_msg(event.user_id, msg)
 
             print('Text: ', event.text)
             print("-------------------")
